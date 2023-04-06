32c1aa71fc4f1fc75012cc9dca734758eb7f8607
Didiloy <didiloy2001@gmail.com>
2023-04-06 11:51:51 +0200

    created the productDetail.html.twig view created the ProductController.php controller

--- PROJET/src/Controller/ProductController.php
+++ PROJET/src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,16 @@ class ProductController extends AbstractController
     #[Route('/product', name: 'app_product')]
     public function index(Request $request): Response
     {
+        //Dummy datas
         $args = [];
+        $args["products"] = [];
+        for ($i = 0; $i < 4; $i++) {
+            $product = new Product();
+            $product->setPrix($i*5.5);
+            $product->setName("produit " . $i);
+            $product->setQuantity($i);
+            array_push($args["products"], $product);
+        }
         return $this->render('products/productList.html.twig', $args);
     }
 }
